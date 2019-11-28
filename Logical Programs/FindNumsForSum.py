079fbaba716aa59d99834ba0486445f92300c0e6
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-11-28 18:31:12 +0530

    Refactored: RollDie for minimum steps

--- Logical Programs/FindNumsForSum.py
+++ Logical Programs/FindNumsForSum.py
@@ -1,8 +1,8 @@
 """Find pairs of numbers adding to given sum"""
 
-arr= [10, 20, 25, 30, 40, 50, 60]
+arr = [10, 20, 25, 30, 40, 50, 60]
 print(arr)
-summation= int(input('Enter sum to find  numbers:'))
+summation = int(input('Enter sum to find  numbers:'))
 
 for i in range(len(arr)-1):
     for j in range(i+1, len(arr)):
--- Logical Programs/RollDie.py
+++ Logical Programs/RollDie.py
@@ -6,29 +6,21 @@ n = int(input('Enter number of times you want to roll a die:'))
 result = [random.randint(1, 6) for i in range(n)]
 print(result)
 
-unique = []
-for r in result:
-    if r not in result:
-        unique.append(r)
-
-
 max_count = 0
-unique = []
 counts = []
-for r in result:
-    count = 0
-    if r not in unique:
-        unique.append(r)
-        for k in result:
-            if k == r:
-                count += 1
-        counts.append(count)
-        if count > max_count:
-            max_count = count
 
-for j in range(len(unique)):
-    if counts[j] == max_count:
-        print(unique[j], end=' ')
+for i in range(1, 7):
+    count = 0
+    for k in result:
+        if k == i:
+            count += 1
+    counts.append(count)
+    if count > max_count:
+        max_count = count
+
+for j in range(1, 7):
+    if counts[j-1] == max_count:
+        print(j, end=' ')
 
 print(f'occurred maximum {max_count} times')
 
