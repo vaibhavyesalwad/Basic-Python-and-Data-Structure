81fa0451e2dd54678e2d288587026ddf1c12b28c
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-11-29 10:59:42 +0530

    Commenting

--- Logical Programs/FindNumsForSum.py
+++ Logical Programs/FindNumsForSum.py
@@ -4,7 +4,7 @@ arr = [10, 20, 25, 30, 40, 50, 60]
 print(arr)
 summation = int(input('Enter sum to find  numbers:'))
 
-for i in range(len(arr)-1):
+for i in range(len(arr)-1):                 # this block find combinations of 2 terms
     for j in range(i+1, len(arr)):
         if arr[i]+arr[j] == summation:
             print(arr[i], arr[j])
