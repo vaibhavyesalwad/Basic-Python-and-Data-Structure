4a364b394bc5b7972480d3285f5be328ede61240
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-11-30 12:27:32 +0530

    Added: Docstrings for functions

--- Logical Programs/StringAnagram.py
+++ Logical Programs/StringAnagram.py
@@ -1,7 +1,8 @@
 """Check whether two words are anagrams are not"""
 
 
-def count(ch, w):             # Find frequency of character
+def count(ch, w):
+    """Function returns occurrences of character in word"""
     i = 0
     for j in w:
         if j == ch:
@@ -10,6 +11,7 @@ def count(ch, w):             # Find frequency of character
 
 
 def anagram(w1, w2):
+    """Function returns boolean value after checking anagram condition for two words"""
     for ch in w1:
         if ch not in w2 or count(ch, w1) != count(ch, w2):
             return False
