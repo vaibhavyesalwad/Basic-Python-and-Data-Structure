1e65eedd79e9c41622ea9b48622045297f969121
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-02 15:43:01 +0530

    New Program: 14_ListFilesInDir lists files & folders in directory

---
+++ Basic Python/14_ListFilesInDir.py
@@ -0,0 +1,3 @@
+"""Program to list all files in a directory in Python"""
+import os               # using os module
+os.system('ls')  # os.system runs single linux command and 'ls' command lists all files and folders in current directory
