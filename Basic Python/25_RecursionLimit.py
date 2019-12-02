66b603941b4bcf28e23a065f143eead938a068f4
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-02 19:37:00 +0530

    Added: comment for help

--- Basic Python/25_RecursionLimit.py
+++ Basic Python/25_RecursionLimit.py
@@ -1,3 +1,5 @@
 """Program to get current value of recursion limit"""
 import sys                      # use sys module
-print(sys.getrecursionlimit())    # sys.getrecursionlimit() gives recursion limit
+print(sys.getrecursionlimit())    # sys.getrecursionlimit() gives recursion limit by default 1
+# can be set by sys.setrecursionlimit()  as per our need
+
