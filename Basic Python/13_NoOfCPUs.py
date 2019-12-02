b731828d27ce6cf6cdcd4ba781559c3c57277ccb
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-02 15:35:41 +0530

    New Program: 13_NoOfCPUs of the system

---
+++ Basic Python/13_NoOfCPUs.py
@@ -0,0 +1,3 @@
+"""Program to find out the number of CPUs using"""
+import os                   # using os module
+os.system('nproc')              # os.system run single command & 'nproc' linux command gives total CPUs in system
