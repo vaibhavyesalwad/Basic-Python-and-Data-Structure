afa3406cfbbd5992db0859238022be18d060c881
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-02 10:52:33 +0530

    Refactored: string formatting and file renamed

--- Basic Python/01_FirstLastName.py
+++ Basic Python/01_FirstLastName.py
@@ -1,7 +1,6 @@
 """1. Program which accepts user's first and last name and print them in reverse order with a space between them. """
 
 name = input('Enter first name and last name:')
-fname, lname = name.split()           # creating  string for output
-full_name = lname + ' ' + fname
-print(full_name)
+fname, lname = name.split()
+print(f'{lname} {fname}')        # creating  string for output
 
