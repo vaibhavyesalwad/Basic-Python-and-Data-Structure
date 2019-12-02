c3ece38f02d7bf49c952259eb7e0190716956404
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-02 19:15:44 +0530

    New Program: 23_BuiltInModules lists all built in modules

---
+++ Basic Python/23_BuiltInModules.py
@@ -0,0 +1,3 @@
+"""Program to find the available built-in modules"""
+import sys                              # using sys module
+print(sys.builtin_module_names)         # sys.builtin_module_names gives all built-in modules
