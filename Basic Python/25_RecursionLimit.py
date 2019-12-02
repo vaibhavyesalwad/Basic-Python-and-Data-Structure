1614b5bc46340e532222b294ffefcd9746930afb
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-02 19:34:01 +0530

    New Program: 25_RecursionLimit

---
+++ Basic Python/25_RecursionLimit.py
@@ -0,0 +1,3 @@
+"""Program to get current value of recursion limit"""
+import sys                      # use sys module
+print(sys.getrecursionlimit())    # sys.getrecursionlimit() gives recursion limit
