2d2d02f5d410a8245d7a42076b12be29b2b07495
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-02 11:10:38 +0530

    New Program: 03_DisplayFirstLast element from list

---
+++ Basic Python/03_DisplayFirstLast.py
@@ -0,0 +1,3 @@
+"""Program to display the first and last colors from the color_list"""
+color_list = ["Red", "Green", "White", "Black"]
+print(color_list[0], color_list[-1])                  # Used negative index
