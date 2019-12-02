f9b9a466e88146ce1e4438c13a9d770b08a20a1d
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-02 16:07:02 +0530

    New Program: 16_CurrentUsnm display usnm of the system

---
+++ Basic Python/16_CurrentUsnm.py
@@ -0,0 +1,3 @@
+"""Program to get the current username """
+import getpass                  # using getpass module
+print(getpass.getuser())               # getpass.getuser()  gives username of the system
