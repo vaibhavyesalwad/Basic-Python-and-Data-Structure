8bc75e688c3f471c163556cb50be541cdebcda2c
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-03 19:07:25 +0530

    New Program: 11_NestedDictFromList created nested dictionary from list elements

---
+++ Python Data Structure/Dictionary/11_NestedDictfromList.py
@@ -0,0 +1,11 @@
+"""Program to convert a list into a nested dictionary of keys"""
+numbers = [1, 2, 3, 4, 5]
+for num in numbers:
+    dict1 = dict.fromkeys([num])                # creating dictionary in each iteration
+    if num == 1:
+        mydictionary = dict1                    # parent dictionary
+    if num > 1:
+        dict2[num-1] = dict1               # assigning new dictionary crated as value for dictionary in last iteration
+    dict2 = dict1
+
+print(mydictionary)
