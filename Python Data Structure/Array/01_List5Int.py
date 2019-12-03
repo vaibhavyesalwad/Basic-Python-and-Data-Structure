6c617082a859cab99255786962e8419f4bf8ae20
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-03 15:43:51 +0530

    Refactored: List to array from array module

--- Python Data Structure/Array/01_List5Int.py
+++ Python Data Structure/Array/01_List5Int.py
@@ -1,4 +1,5 @@
 """Program to create an array of 5 integers and display the array items. Access individual element through indexes"""
-numbers = [5, 4, 3, 2, 1]             # Created list/array of 5 integers
+import array as arr
+numbers = arr.array('I', [5, 4, 3, 2, 1])             # Created list/array of 5 integers
 for i in range(len(numbers)):
     print(numbers[i])                 # accessing elements of list using index
