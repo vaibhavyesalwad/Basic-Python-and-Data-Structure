b4ae04e716afc4a5f1617b52f648018d328e6f99
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-03 17:14:32 +0530

    Added: another approaches to iterate dictionary

--- Python Data Structure/Dictionary/04_IterateDictLoop.py
+++ Python Data Structure/Dictionary/04_IterateDictLoop.py
@@ -2,3 +2,13 @@
 dict1 = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 for key, value in dict1.items():  # iterate dict.items() in for loop it will give key & value
     print(key, value)
+
+'''
+for item in dict1.items():         # prints items in dictionary
+    print(item)
+'''
+
+'''
+for item in dict1:
+    print(item)                # prints only keys
+'''
