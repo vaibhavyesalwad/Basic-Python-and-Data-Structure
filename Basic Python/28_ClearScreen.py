7ba398f4e0a827fd9e088978437134449b3149cb
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-03 10:12:39 +0530

    New Program: 28_ClearScreen clears screen or terminal

---
+++ Basic Python/28_ClearScreen.py
@@ -0,0 +1,5 @@
+"""Program to clear the screen or terminal"""
+import os, time
+os.system('ls')                             # lists files in directory
+time.sleep(3)                              # adds  delay by 3 seconds
+os.system('clear')                    # clears screen
