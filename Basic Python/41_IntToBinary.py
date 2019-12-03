5fa14077abafe8b7c98f97cbeae62ffb0fd79a18
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-03 13:54:54 +0530

    New Program: 41_IntToBinary retaining leading zeros

---
+++ Basic Python/41_IntToBinary.py
@@ -0,0 +1,4 @@
+"""Program to convert an integer to binary keep leading zeros"""
+num = 50
+print(format(num, "010b"))          # using format function
+print(format(num, "012b"))     # total digits 10 & 12 these are right aligned
