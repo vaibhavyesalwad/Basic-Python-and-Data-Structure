e6d04f736676e62cec3b5c838164ab922bfaffa6
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-03 19:31:33 +0530

    New Program: 12_CheckMultipleKeys whether keys are present or not

---
+++ Python Data Structure/Dictionary/12_CkeckMultipleKeys.py
@@ -0,0 +1,7 @@
+"""Program to check multiple keys exists in a dictionary"""
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+# using membership operator 'in' for iterables & in dictionary it checks keys
+print(f'If z & b are present as keys: {all(key in dict1 for key in ("z", "b"))}')
+# all() checks if all entries are True
+print(f'If c & b are present as keys: {all(key in dict1 for key in ("c", "b"))}')
++
