f74230ee862f2fe1e75eebbcf70f44d915af7f7c
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-03 15:37:52 +0530

    New Program: 01_List5Int creating list of 5 integers & access through indices

---
+++ Python Data Structure/Array/01_List5Int.py
@@ -0,0 +1,4 @@
+"""Program to create an array of 5 integers and display the array items. Access individual element through indexes"""
+numbers = [5, 4, 3, 2, 1]             # Created list/array of 5 integers
+for i in range(len(numbers)):
+    print(numbers[i])                 # accessing elements of list using index
