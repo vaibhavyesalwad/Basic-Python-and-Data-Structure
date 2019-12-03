6af7033fbab3d92d4883b8257c83a238608d4924
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-03 17:07:52 +0530

    New Program: 04_IterateDictLoop printing key and value

---
+++ Python Data Structure/Dictionary/04_IterateDictLoop.py
@@ -0,0 +1,4 @@
+"""Program to iterate over dictionaries using for loops"""
+dict1 = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+for key, value in dict1.items():  # iterate dict.items() in for loop it will give key & value
+    print(key, value)
