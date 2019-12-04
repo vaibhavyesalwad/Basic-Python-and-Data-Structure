3e3d1dda8ec1cff3f9a61069e46b885c3f62686c
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 10:42:57 +0530

    Refactored: Used set comprehension over loops

--- Python Data Structure/Dictionary/07_PrintUniqueValues.py
+++ Python Data Structure/Dictionary/07_PrintUniqueValues.py
@@ -2,9 +2,5 @@
 mylist = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"},
           {"VII": "S005"}, {"V": "S009"}, {"VIII": "S007"}]      # sample list of data
 
-unique = set()
-for element in mylist:                         # iterate in given list
-    for key, value in element.items():
-        unique.add(value)                               # taking out only values from dictionaries
-
+unique = {value for element in mylist for key, value in element.items()}    # used set comprehension
 print(f'Unique values: {unique}')
