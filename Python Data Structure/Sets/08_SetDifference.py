10de7e124b35c65d704b2248b7e483631d8999c1
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 12:39:06 +0530

    Added: SetDifference- printing difference of 2 sets

---
+++ Python Data Structure/Sets/08_SetDifference.py
@@ -0,0 +1,4 @@
+"""Program to create set difference"""
+a = {1, 2, 3, 4}                    # created sets
+b = {4, 5, 1, 7, 8, 9}
+print(f'Difference of sets a & b & c: {a-b}')  # set difference gives elements in set that are not in other set
