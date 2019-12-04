1e8b26903f5f17d24697f0e724237cc69380c711
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 19:25:48 +0530

    Refactored: tried another approach removing elements having count more than 1

--- Python Data Structure/List/17_RemoveDuplicatesListOfLists.py
+++ Python Data Structure/List/17_RemoveDuplicatesListOfLists.py
@@ -1,7 +1,7 @@
 """Program to remove duplicates from a list of lists"""
 data = [[10, 20], [40], [30, 56, 25], [10, 20], [33], [40]]
-data1 = []
 for d in data:
-    if d not in data1:
-        data1.append(d)
-print(f'After removing duplicates: {data1}')
+    if data.count(d) > 1:
+        data.remove(d)
+
+print(f'After removing duplicates: {data}')
