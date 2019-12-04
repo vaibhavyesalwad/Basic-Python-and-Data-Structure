34b34fe16ff26510563b7f2219c27292e871d128
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 11:50:03 +0530

    Refactored: Changed variable name mydictionary to my_dictionary

--- Python Data Structure/Dictionary/11_NestedDictfromList.py
+++ Python Data Structure/Dictionary/11_NestedDictfromList.py
@@ -3,9 +3,9 @@ numbers = [1, 2, 3, 4, 5]
 for num in numbers:
     dict1 = dict.fromkeys([num])                # creating dictionary in each iteration
     if num == 1:
-        mydictionary = dict1                    # parent dictionary
+        my_dictionary = dict1                    # parent dictionary
     if num > 1:
         dict2[num-1] = dict1               # assigning new dictionary crated as value for dictionary in last iteration
     dict2 = dict1
 
-print(mydictionary)
+print(my_dictionary)
