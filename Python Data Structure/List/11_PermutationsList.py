5cb371821c6dfad98b429b4f234a38653aaaf4db
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 16:37:20 +0530

    Added: Info about permutations & combinations of itertools module

--- Python Data Structure/List/11_PermutationsList.py
+++ Python Data Structure/List/11_PermutationsList.py
@@ -2,3 +2,9 @@
 import itertools                               # using itertools module
 numbers = [10, 20, 30, 40, 50]                               # itertools.permutations gives object
 print(f'All possible permutations:\n {list(itertools.permutations(numbers))}')
+
+# we should give input in set of numbers i.e. unique as it considers positions as unique
+'''
+list(itertools.permutations(mylist, n)   # gives permutations of length n for elements of mylist 
+list(itertools.combinations(mylist, n)   # gives combinations of length n for elements of mylist
+'''
