37ce302325332944d3101f6349ee0f7e9407d326
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 10:26:28 +0530

    Refactored: used dictionary comprehension instead of loop

--- Python Data Structure/Dictionary/08_DictFromString.py
+++ Python Data Structure/Dictionary/08_DictFromString.py
@@ -1,7 +1,4 @@
 """Program to create a dictionary from a string"""
 string = 'w3resource'
-dict1 = {}                             # created empty dictionary
-for ch in string:
-    dict1[ch] = string.count(ch)        # putting items to  dictionary
-
+dict1 = {ch: string.count(ch) for ch in string}       # Used dictionary comprehension for creating dictionary
 print(dict1)
