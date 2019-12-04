c082a12d5d0c42731752e4ad2ea2db67f34f5d23
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 12:22:22 +0530

    Added: IntersectionSet- printing common elements from two sets

---
+++ Python Data Structure/Sets/06_IntersectionSet.py
@@ -0,0 +1,4 @@
+"""Program to create an intersection of sets"""
+a = {1, 2, 3, 4}
+b = {4, 5, 1, 7, 8, 9}
+print(f'Intersection of sets a & b is: {a & b}')  # intersection gives common elements from two sets
