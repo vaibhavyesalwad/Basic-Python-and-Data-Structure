84e8e0a179cc43360ebc28c0f1ed9b621ce99da1
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 11:02:25 +0530

    Refactored: Used list comprehension over loops

--- Python Data Structure/Dictionary/09_PrintDictInTable.py
+++ Python Data Structure/Dictionary/09_PrintDictInTable.py
@@ -1,11 +1,9 @@
 """Program to print a dictionary in table format"""
-dict1 = {'a': [1, 2, 3, 4], 'b': [2, 3, 4, 5], 'c': [3, 4, 5, 6], 'd': [4, 5, 6, 7]}
-values = []
-for key, value in dict1.items():           # getting values of dictionary
-    print(key, end=' ')                      # printing keys
-    values.append(value)
-print()
-for i in range(len(value)):
+dict1 = {'a': [1, 2, 3, 4], 'b': [2, 3, 4, 5], 'c': [3, 4, 5, 6], 'd': [4, 5, 6, 7], 'e': [5, 6, 7, 8]}
+values = [value for key, value in dict1.items()]      # used list comprehension
+for d in dict1:
+    print(d, end=' ')
+for i in range(len(values[0])):                     # iterate till end of each value i.e. list
+    print()
     for ch in values:
         print(ch[i], end=' ')                 # printing values in columns of it's key
-    print()
