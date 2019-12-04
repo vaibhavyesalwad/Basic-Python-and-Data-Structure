71a92176e2b0cd12523ba8adf8cad9bc85f3caf2
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 19:17:09 +0530

    Added: comment for usage of sum fn & generator expression

--- Python Data Structure/Dictionary/10_CountSuccessValues.py
+++ Python Data Structure/Dictionary/10_CountSuccessValues.py
@@ -1,6 +1,6 @@
 """Program to count the values associated with key in a dictionary"""
 mylist = [{'id': 1, 'success': True, 'name': 'Lary'},  {'id': 2, 'success': False, 'name': 'Rabi'},
            {'id': 3, 'success': True, 'name': 'Alex'}]
-
-count = sum(element['success'] for element in mylist)   # Adding value for success key as True is 1 & False is 0
+# sum fn on generator expressions, sum fn access each element
+count = sum(element['success'] for element in mylist)  # Adding value for success key as True is 1 & False is 0
 print(f'{count} have success as True')
--- Python Data Structure/Dictionary/13_CountValuesAsList.py
+++ Python Data Structure/Dictionary/13_CountValuesAsList.py
@@ -1,5 +1,5 @@
 """Program to count number of items in a dictionary value that is a list"""
 dict1 = {'a': [1, 2, 3, 4], 'b': [2, 3, 4, 5], 'c': 1, 'd': 'z', 'e': (1,)}
 # if value is list added True i.e. 1 else False i.e. 0
-count = sum(isinstance(value, list) for value in dict1.values())      # sum fn & tuple comprehension
+count = sum(isinstance(value, list) for value in dict1.values())      # sum fn & generator expression
 print(f'{count} values are list')
