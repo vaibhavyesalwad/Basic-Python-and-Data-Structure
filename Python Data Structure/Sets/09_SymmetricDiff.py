68c9d678dfb9952f7b21c3b40a360877643e9380
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 12:43:58 +0530

    Added: SetDifference- printing symmetric difference of 2 sets

---
+++ Python Data Structure/Sets/09_SymmetricDiff.py
@@ -0,0 +1,4 @@
+"""Program to create a symmetric difference"""
+a = {1, 2, 3, 4}
+b = {4, 5, 1, 7, 8, 9}
+print(f'Symmetric difference of sets a & b: {a^b}')  # symmetric diff is other than common in union of two sets
