4c03766ae2c04e173b7289117d0b56ecd69cdaac
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 11:16:59 +0530

    Refactored: Used sum fn and tuple comprehension

--- Python Data Structure/Dictionary/10_CountSuccessValues.py
+++ Python Data Structure/Dictionary/10_CountSuccessValues.py
@@ -2,5 +2,5 @@
 mylist = [{'id': 1, 'success': True, 'name': 'Lary'},  {'id': 2, 'success': False, 'name': 'Rabi'},
            {'id': 3, 'success': True, 'name': 'Alex'}]
 
-count = sum(tuple(element['success'] for element in mylist))   # Adding value for success key as True is 1 & False is 0
+count = sum(element['success'] for element in mylist)   # Adding value for success key as True is 1 & False is 0
 print(f'{count} have success as True')
--- Python Data Structure/Dictionary/13_CountValuesAsList.py
+++ Python Data Structure/Dictionary/13_CountValuesAsList.py
@@ -1,7 +1,5 @@
 """Program to count number of items in a dictionary value that is a list"""
 dict1 = {'a': [1, 2, 3, 4], 'b': [2, 3, 4, 5], 'c': 1, 'd': 'z', 'e': (1,)}
-count = 0
-for value in dict1.values():             # iterate through values of dictionary
-    count += isinstance(value, list)        # if value is list added True i.e. 1 else False i.e. 0
-
+# if value is list added True i.e. 1 else False i.e. 0
+count = sum(isinstance(value, list) for value in dict1.values())      # sum fn & tuple comprehension
 print(f'{count} values are list')
