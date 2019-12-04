c5be69f668b5cda226d6eb31dc4465abb4e51bc8
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 12:52:29 +0530

    Added: ClearSet- clearing set i.e making it empty

---
+++ Python Data Structure/Sets/10_ClearSet.py
@@ -0,0 +1,5 @@
+"""Program to clear a set"""
+myset = {1, 2, 3, 4}
+print(f'myset before clearing: {myset}')
+myset.clear()                   # removes all elements and make it empty set
+print(f'after clearing: {myset}')
