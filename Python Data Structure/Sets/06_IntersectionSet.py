afd0023c3f5c7fef1e39e44953ea86934de5f205
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 12:32:40 +0530

    Added: new 3rd set c & done intersection of 3 sets

--- Python Data Structure/Sets/06_IntersectionSet.py
+++ Python Data Structure/Sets/06_IntersectionSet.py
@@ -1,4 +1,5 @@
 """Program to create an intersection of sets"""
 a = {1, 2, 3, 4}
 b = {4, 5, 1, 7, 8, 9}
-print(f'Intersection of sets a & b is: {a & b}')  # intersection gives common elements from two sets
+c = {3, 4, 5, 6}
+print(f'Intersection of sets a & b is: {a & b & c}')  # intersection gives common elements from two sets
