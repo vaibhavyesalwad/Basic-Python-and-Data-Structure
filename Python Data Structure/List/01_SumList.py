913eb417363c3c16dca4deb5a32972b6888ea655
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 13:28:28 +0530

    Added: SumList- printing sum of elements in list

---
+++ Python Data Structure/List/01_SumList.py
@@ -0,0 +1,3 @@
+"""Program to sum all the items in a list"""
+numbers = [10, 11, 12, 13, 14]         # created list
+print(f'Sum: {sum(numbers)}')       # sum fn takes iterables & return sum of elements
