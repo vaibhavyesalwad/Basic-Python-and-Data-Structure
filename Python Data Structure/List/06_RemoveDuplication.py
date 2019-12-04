c4a6d3e6ea9f742e125ff05eb3d80cf4f8ce3606
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-04 14:32:38 +0530

    Added: RemoveDuplication- printing unique elements

---
+++ Python Data Structure/List/06_RemoveDuplication.py
@@ -0,0 +1,4 @@
+"""Program to remove duplicates from a list"""
+numbers = [10, 15, 15, 20, 50, 55, 65, 20, 30, 50]
+print(f'Unique elements: {set(numbers)}')        # set accepts only unique elements so typecasting to set
+
