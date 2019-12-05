9044a8750db93c4a0b974651570ad89618ac7ef2
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-05 11:37:25 +0530

    Added: CharFrequency- counting occurences of character in string & creating dictionary for it

--- Python Data Structure/Strings/02_CharFrequency.py
+++ Python Data Structure/Strings/02_CharFrequency.py
@@ -1,3 +1,3 @@
 """Program to count the number of characters (character frequency) in a string"""
-string = 'Mumbai is the financial capital of India'
-print(f"Frequency of 'a': {string.count('a')} ")
+string = 'google.com'
+print(f"Frequency dictionary: {({ch: string.count(ch) for ch in string})}")        # used dictionary comprehension
