9c3f0e94070bc9647e4c63885540b274b44a336a
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-05 11:14:21 +0530

    Added: Slicing- printing parts of tuple using slicing

---
+++ Python Data Structure/Tuple/09_Slicing.py
@@ -0,0 +1,5 @@
+"""Program to slice a tuple"""
+tuple1 = (1, 2, 3, 4, 5)
+print(f'Slicing tuple & showing only first two items:{tuple1[0:2]}')       # slicing means accessing parts of tuple
+print(f'Using negative index & showing last two items:{tuple1[-2:]}')
+print(f'Showing reverse of tuple: {tuple1[::-1]}')        # reversing order for display
