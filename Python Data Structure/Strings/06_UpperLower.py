315f8224ebc64491d08f77f9142dbd012543de03
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-05 12:36:27 +0530

    Added: UpperLower- printing upper case and lower case of string

---
+++ Python Data Structure/Strings/06_UpperLower.py
@@ -0,0 +1,5 @@
+"""Write script that takes input from the user and displays that input back in upper and lower cases"""
+string = input('Enter string:')
+print(f'Upper case:{string.upper()} Lower case: {string.lower()}')
+# str.upper() gives string in upper case and str.lower() gives string in lower case
+
