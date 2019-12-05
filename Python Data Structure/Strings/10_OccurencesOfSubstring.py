76dfa687c255296449fd26744ed27f0eb1689385
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-05 15:04:32 +0530

    Added: OccurencesOfSubstring- counting occurrences of substring

---
+++ Python Data Structure/Strings/10_OccurencesOfSubstring.py
@@ -0,0 +1,3 @@
+"""Program to count occurrences of a substring in a string"""
+string = 'abcbcnalancbacabxabaaacabbcfncamabcmaabcandnamanabcnepe'
+print(f"Occurences of 'abc': {string.count('abc')}")    # str.count(ch) gives count of occurrences of ch in given string
