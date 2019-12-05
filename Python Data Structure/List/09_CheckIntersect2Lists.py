9c99991e1111364a8f42b33245f993e536665056
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-05 15:33:41 +0530

    Refactored: used 'any' function instead of loop

--- Python Data Structure/List/09_CheckIntersect2Lists.py
+++ Python Data Structure/List/09_CheckIntersect2Lists.py
@@ -3,12 +3,9 @@
 
 def check_common(list1, list2):
     """Function returns True if two given lists have common element(s) else returns False"""
-    for item in list1:
-        if item in list2:
-            return True
-    return False
+    return any(num in list2 for num in list1)
 
 
 nums1 = [1, 2, 3, 4, 5]
-nums2 = [6, 7, 8, 9, 1]
+nums2 = [6, 7, 8, 9, 3]
 print(f'Do lists have common element(s)? {check_common(nums1, nums2)}')
