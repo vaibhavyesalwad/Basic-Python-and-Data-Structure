6aadb699273e9be1aecdfb1f2d63d682aa9c770b
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-05 11:28:05 +0530

    Added: CharFrquency- count occurrences of character in string

---
+++ Python Data Structure/Strings/02_CharFrequency.py
@@ -0,0 +1,3 @@
+"""Program to count the number of characters (character frequency) in a string"""
+string = 'Mumbai is the financial capital of India'
+print(f"Frequency of 'a': {string.count('a')} ")
