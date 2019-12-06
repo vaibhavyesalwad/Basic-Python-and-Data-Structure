933aaa70e282a1b63aba64ee51d08f08f3bf7d60
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-06 10:01:11 +0530

    Refactored: Used membership operator 'in' directly

--- Basic Python/07_CheckValueInList.py
+++ Basic Python/07_CheckValueInList.py
@@ -1,14 +1,4 @@
 """ Program to check whether a specified value is contained in a group of values in Test Data"""
-
-
-def check_value(number, sequence):
-    """Checks whether given number is present in given list/sequence of numbers"""
-    if number in sequence:
-        return True
-    return False
-
-
 test_data = [1, 5, 8, 3]
-print('test data:', test_data)
 num = int(input('Enter number to check in test data:'))
-print(check_value(num, test_data))         # prints returning value of function
+print(f'Is {num} in {test_data}: {num in test_data}')         # prints returning value of function
