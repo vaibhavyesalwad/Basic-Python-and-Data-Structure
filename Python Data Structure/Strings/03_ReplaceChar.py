ed67269c367df4f54310a9d48c2b31dd79749538
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-06 10:00:11 +0530

    Refactored: Made solution dynamic i.e. for any string

--- Python Data Structure/Strings/03_ReplaceChar.py
+++ Python Data Structure/Strings/03_ReplaceChar.py
@@ -1,5 +1,7 @@
 """program to get a string from a given string where all occurrences of its first char have been
 changed to '$', except the first char itself"""
 string = 'restart'
-new = string.replace('r', '$')               # using str.replace() method returns new string with all replacements
-print(new.replace('$', 'r', 1))                # if count given replace that many characters only
+ch = 'r'
+i = string.index(ch)                # using str.replace() method returns new string with all replacements
+print(string[:i+1]+string[i+1:].replace(ch, '$'))
+# count given replace that many characters only
